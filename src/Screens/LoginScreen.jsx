88dac77e6c251abb216a2cf43b624e0959d680a1
Andrew Mather <Andy-mather@live.co.uk>
2022-03-08 12:04:53 +0000

    log in screen

---
+++ src/Screens/LoginScreen.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const LoginScreen = () => {
+    return (
+        <View style={styles.center}>
+            <Text >
+                login screen
+            </Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    center: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        textAlign: "center",
+    },
+});
+
+
+export default LoginScreen;
