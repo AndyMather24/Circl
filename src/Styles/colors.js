84fabfb586ea82f9f17d105bdbf8f8c6233159fb
Andrew Mather <Andy-mather@live.co.uk>
2022-03-10 13:15:19 +0000

    added dedicated style sheets for re-use

---
+++ src/Styles/colors.js
@@ -0,0 +1,8 @@
+
+export const colors = {
+    primaryFont: "#FFFFFF",
+    secondaryFont: "#818190", 
+    primaryBackground: "#020018",
+    componentBackground: "#1B192F",
+    secondaryComponentBackground: "#0F0D24"
+}
---
+++ src/Styles/typography.js
@@ -0,0 +1,42 @@
+import { StyleSheet } from 'react-native'
+import * as Colors from "./colors"
+
+
+
+
+export const Header = {
+
+    main: {
+        fontSize: 26,
+        fontWeight: "bold",
+        color: Colors.colors.primaryFont
+    },
+
+    title: {
+        fontSize: 16,
+        fontWeight: "bold",
+        color: Colors.colors.primaryFont
+    }
+
+};
+
+export const Transform = {
+    upperCase: {
+        textTransform: "uppercase"
+    }
+};
+
+export const FontSize = {
+    sm: {
+        fontSize: 13
+    }
+};
+
+export const FontWeight = {
+    bold:{
+        fontWeight: "bold"
+    } 
+};
+
+
+
