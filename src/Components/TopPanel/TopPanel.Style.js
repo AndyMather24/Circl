a1863a85548bfae27423e236e222f7423590620e
Andrew Mather <Andy-mather@live.co.uk>
2022-03-13 11:47:11 +0000

    top panel collapses

--- src/Components/TopPanel/TopPanel.Style.js
+++ src/Components/TopPanel/TopPanel.Style.js
@@ -1,13 +1,19 @@
 import { StyleSheet } from 'react-native'
-import { Typography } from '../../Styles';
+import { backgroundColor } from 'react-native/Libraries/Components/View/ReactNativeStyleAttributes';
+import { Colors, Typography } from '../../Styles';
 
 const styles = StyleSheet.create({
     top: {
         height: 230,
         backgroundColor: "#1B192F",
         borderTopLeftRadius: 20,
-        borderTopRightRadius: 20,
+        borderTopRightRadius: 20
     },
+    hideTop : {
+        height: 100,
+        backgroundColor: "#1B192F",
+    },
+
     header: {
         ...Typography.Header.main,
         marginTop: 55,
@@ -15,10 +21,17 @@ const styles = StyleSheet.create({
         marginBottom: 10,
     },
 
-    flexRow: {
-        flexDirection: 'column',
-        flex: 1
+    headerRow: {
+        flexDirection: 'row',
+        height:100
+    }, 
 
+    chevron: {
+        ...Typography.FontSize.md,
+        color: Colors.colors.primaryFont,
+        marginTop: 55,
+        marginLeft: 240,
+        marginBottom: 10,
     }
 })
 
--- src/Components/TopPanel/TopPanel.jsx
+++ src/Components/TopPanel/TopPanel.jsx
@@ -1,27 +1,44 @@
-import { View, Text, ScrollView} from 'react-native'
+import React, { useState } from 'react';
+import { View, Text, ScrollView, TouchableOpacity } from 'react-native';
 import PromoCard from '../PromoCard/PromoCard';
-import { styles } from './TopPanel.Style'
+import { styles } from './TopPanel.Style';
 import FixtureCard from '../FixtureCard/FixtureCard';
+import TabBarIcon from '../TabBarIcon';
+const TopPanel = ({ navigation, matches }) => {
+  const [IsVisable, SetIsVisable] = useState(true);
 
-const TopPanel = ({navigation, matches}) => {
-        return (
-        
-            <View style={styles.top}>
-                    <Text style={[styles.header]}>Fixtures</Text>
-                <ScrollView horizontal>
-                    <PromoCard/>
-                  { matches.map((match, i) => {
-                        return (
-                             <FixtureCard key={i}  navigation={navigation} match={match}></FixtureCard>
-                    )
-                     })
-                    }
-                </ScrollView>
-            </View>
-        )
+  return (
+    <View style={IsVisable ? styles.top : styles.hideTop}>
+      <View style={styles.headerRow}>
+        <Text style={styles.header}>Fixtures</Text>
+        <TouchableOpacity onPress={TogglePanel} style={styles.chevron}>
+          {DisplayChevron(IsVisable)}
+        </TouchableOpacity>
+      </View>
+      {renderPanel(IsVisable)}
+    </View>
+  );
+
+  function TogglePanel() {
+    SetIsVisable(!IsVisable);
+  }
+
+  function DisplayChevron(isVisable) {
+    return isVisable == true ? <TabBarIcon name="chevron-up" /> : <TabBarIcon name="chevron-down" />;
+  }
+
+  function renderPanel(isVisable) {
+    if (isVisable) {
+      return (
+        <ScrollView horizontal>
+          <PromoCard />
+          {matches.map((match, i) => {
+            return <FixtureCard key={i} navigation={navigation} match={match}></FixtureCard>;
+          })}
+        </ScrollView>
+      );
     }
-
-  
+  }
+};
 
 export default TopPanel;
-
